1a528e877f0253bca9487ab9a11d46a2b261e062
Andres Toro <carlostoro04@gmail.com>
2020-04-08 22:58:42 -0500

    image database model and implement Menu component using framer-motion package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
 import Header from '../layouts/header'
+import { Menu } from '../src/components/Menu';
+import "../src/styles/styles.css";
 
 export default function Index () {
     return (
-        <div>
-            <Header/>
-            <nav>
-                <div className="nav-wrapper">
-                    <ul className="right hide-on-med-and-down">
-                        <li><a href="#!"><i className="material-icons">search</i></a></li>
-                        <li><a href="#!"><i className="material-icons">view_module</i></a></li>
-                        <li><a href="#!"><i className="material-icons">refresh</i></a></li>
-                        <li><a href="#!"><i className="material-icons">more_vert</i></a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <Menu/>
     )
 }
