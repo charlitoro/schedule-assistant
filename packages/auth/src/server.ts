1b99fa763b93241ab25c5dca024e218827bb01b4
Andres Toro <carlostoro04@gmail.com>
2020-07-20 18:52:20 -0500

    change url

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -9,7 +9,7 @@ const options:cors.CorsOptions = {
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
     credentials: true,
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: 'http://next.charli.com',
+    origin: 'https://planner.charlitoro.com',
     preflightContinue: false
 };
 
