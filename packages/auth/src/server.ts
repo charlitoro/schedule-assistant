45e2255546bde49a3f074141b3d36200a9d53e33
Andres Toro <carlostoro04@gmail.com>
2020-07-22 21:42:01 -0500

    fix origin in auth server

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -10,7 +10,7 @@ const options:cors.CorsOptions = {
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
     credentials: true,
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: 'http://planner.charlitoro.com',
+    origin: 'https://planner.charlitoro.com',
     preflightContinue: false
 };
 
